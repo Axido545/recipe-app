5b1fb7fa01227a1afe3232e50dbdcf8ae964c7d3
axido545 <axido545@gmail.com>
2024-05-21 22:33:31 +0200

    add header bar

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
 
 export default function Header() {
     return (
         <div>
-            Header
+            <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div class="container">
+                    <Link href={"/"} class="navbar-brand">My Recipes</Link>
+                    <Link href={"/signin"} class="btn btn-outline-success">Sign In</Link>
+                </div>
+            </nav>
         </div>
     )
 }
