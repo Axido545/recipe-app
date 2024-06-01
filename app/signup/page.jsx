6abb0d946262894b8584aa9c91ddf0a4dd176dd0
axido545 <axido545@gmail.com>
2024-06-01 20:58:25 +0200

    page Sign up change jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-
+"use client"
 function SignUp() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
     return (
-        <div>
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <div className='d-flex align-items-center justify-content-center' style={{ "height": "80vh" }}>
+            <form onSubmit={handleSubmit}>
+                <div className='fs-3 my-3 text-center'>
+                    Sign Up
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
