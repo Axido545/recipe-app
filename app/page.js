8e36c69980e31ab4a3cd642d345cfee4f0cfaaf1
axido545 <axido545@gmail.com>
2024-06-02 14:17:19 +0200

    add title and btn to home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="my-3">
+      <div className="d-flex justify-content-between container">
+        <h4>Recipes List</h4>
+        <Link href={"/recipes/create"} className="btn btn-primary">Add New Recipe</Link>
+      </div>
     </main>
   );
 }
