94896d25e6724c81988ef445ef78330201a78c4a
axido545 <axido545@gmail.com>
2024-06-02 19:43:22 +0200

    add => create recipes

--- app/recipes/create/page.jsx
+++ app/recipes/create/page.jsx
@@ -1,7 +1,25 @@
+"use client"
+import { useState } from "react"
 export default function CreateRecipe() {
+    const [title, setTitle] = useState("");
+    const [ingredient, setIngredient] = useState("");
+    const [instructions, setInstructions] = useState("");
+    const [file, setFile] = useState(null)
     return (
         <div>
-
+            <form action="">
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        placeholder="title"
+                    />
+                </div>
+                <div className="mb-3"></div>
+                <div className="mb-3"></div>
+                <div className="mb-3"></div>
+            </form>
         </div>
     )
 }
