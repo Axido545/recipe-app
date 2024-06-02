2d7ca5524a4571f43d7da91a725fba342b4b917e
axido545 <axido545@gmail.com>
2024-06-02 14:16:52 +0200

    add getAuth

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBhZbRievQ7mlov_lebwVvXnp5eErPDb1s",
     authDomain: "recipe-app-fb728.firebaseapp.com",
@@ -13,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth()
