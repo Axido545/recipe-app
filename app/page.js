2011af8c7cefce1b1e2134511159b20996705b4c
axido545 <axido545@gmail.com>
2024-06-02 19:01:32 +0200

    add hooks

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Link from "next/link";
 import RecipeCard from "./components/RecipeCard";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from "firebase"
 
 export default function Home() {
+  const [recipeList, setRecipeList] = useState([])
+
+  useEffect(() => {
+    const getRecipesList = async () => {
+      const recipesCollection = collection(db, "recipes")
+
+    }
+
+  }, [])
+
   return (
     <main className="my-3 container">
       <div className="d-flex justify-content-between">
@@ -12,7 +26,7 @@ export default function Home() {
         <Link href={"/recipes/create"} className="btn btn-primary">Add New Recipe</Link>
       </div>
       <div className="d-flex flex-wrap">
-        <RecipeCard>Recipe Card</RecipeCard>
+        <RecipeCard></RecipeCard>
       </div>
     </main>
   );
