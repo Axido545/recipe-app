c96aca0329306f6c2e5e9c0222abad9cbe29d56e
axido545 <axido545@gmail.com>
2024-06-02 14:33:03 +0200

    add css and component

--- app/page.js
+++ app/page.js
@@ -2,13 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Link from "next/link";
+import RecipeCard from "./components/RecipeCard";
 
 export default function Home() {
   return (
-    <main className="my-3">
-      <div className="d-flex justify-content-between container">
+    <main className="my-3 container">
+      <div className="d-flex justify-content-between">
         <h4>Recipes List</h4>
         <Link href={"/recipes/create"} className="btn btn-primary">Add New Recipe</Link>
+      </div>
+      <div className="d-flex flex-wrap">
+        <RecipeCard>Recipe Card</RecipeCard>
       </div>
     </main>
   );
