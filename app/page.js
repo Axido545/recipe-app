a73df18e77d8a0858501f18d553c8ac5128bf267
axido545 <axido545@gmail.com>
2024-06-02 19:43:01 +0200

    add => read recipes

--- app/page.js
+++ app/page.js
@@ -6,17 +6,23 @@ import Link from "next/link";
 import RecipeCard from "./components/RecipeCard";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from 'firebase/firestore'
-import { db } from "firebase"
+import { db } from "../firebase"
 
 export default function Home() {
-  const [recipeList, setRecipeList] = useState([])
+  const [recipesList, setRecipesList] = useState([])
 
   useEffect(() => {
     const getRecipesList = async () => {
       const recipesCollection = collection(db, "recipes")
-
+      const recipesSnapShot = await getDocs(recipesCollection)
+      const recipes = recipesSnapShot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id
+      }))
+      setRecipesList(recipes);
+      console.log(recipesList)
     }
-
+    getRecipesList();
   }, [])
 
   return (
@@ -26,8 +32,13 @@ export default function Home() {
         <Link href={"/recipes/create"} className="btn btn-primary">Add New Recipe</Link>
       </div>
       <div className="d-flex flex-wrap">
-        <RecipeCard></RecipeCard>
+        {recipesList.map(item => {
+          return item.imageUrl ? < RecipeCard props={item} ></RecipeCard> : null;
+        }
+
+        )}
+
       </div>
-    </main>
+    </main >
   );
 }
